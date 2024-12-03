2497bd940ca1d89e9736202251c9dd8620bceb5e
sammi <sshih5678@gmail.com>
2024-12-02 16:13:29 -0800

    day 2 part 2

--- aoc_2024/solutions/day_02.py
+++ aoc_2024/solutions/day_02.py
@@ -1,4 +1,3 @@
-from time import sleep
 from rich.console import Console
 
 from aoc_2024.utils.get_input_for_day import get_input_for_day
@@ -25,7 +24,31 @@ def part_one(input_data: list[str]):
 
 
 def part_two(input_data: list[str]):
-    answer = ...
+    answer = 0
+
+    def check_safety(levels: list[str]) -> bool:
+        if levels[0] == levels[1]:
+            return False
+        isIncreasing = int(levels[0]) < int(levels[1])
+        for i in range(1, len(levels)):
+            diff = int(levels[i]) - int(levels[i - 1])
+            if isIncreasing and diff < 1 or diff > 3:
+                return False
+            elif not isIncreasing and diff > -1 or diff < -3:
+                return False
+        return True
+
+    for report in input_data:
+        levels = report.split()
+        isSafe = check_safety(levels)
+        if not isSafe:
+            for i in range(len(levels)):
+                new_levels = levels.copy()
+                new_levels.pop(i)
+                isSafe = check_safety(new_levels)
+                if isSafe:
+                    break
+        answer += isSafe
 
     return answer
 
