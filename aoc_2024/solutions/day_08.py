a8f4c3a3c13bf1be39752203c480775ac6e67524
sammi <sshih5678@gmail.com>
2024-12-08 17:33:23 -0800

    day 8 part 2

--- aoc_2024/solutions/day_08.py
+++ aoc_2024/solutions/day_08.py
@@ -36,9 +36,38 @@ def part_one(input_data: list[str]):
 
 
 def part_two(input_data: list[str]):
-    answer = ...
+    height, width = len(input_data), len(input_data[0])
+    antennas_map = {}
+    antinodes = set()
 
-    return answer
+    for i in range(height):
+        for j in range(width):
+            if input_data[i][j] != ".":
+                antennas_map.setdefault(input_data[i][j], []).append((i, j))
+
+    for antennas in antennas_map.values():
+        for i in range(len(antennas) - 1):
+            for j in range(i + 1, len(antennas)):
+                cur_y, cur_x, other_y, other_x = (
+                    antennas[i][0],
+                    antennas[i][1],
+                    antennas[j][0],
+                    antennas[j][1],
+                )
+                delta_y = other_y - cur_y
+                delta_x = other_x - cur_x
+                next_y, next_x = cur_y, cur_x
+                while 0 <= next_y < height and 0 <= next_x < width:
+                    antinodes.add((next_y, next_x))
+                    next_y -= delta_y
+                    next_x -= delta_x
+                next_y, next_x = cur_y + delta_y, cur_x + delta_x
+                while 0 <= next_y < height and 0 <= next_x < width:
+                    antinodes.add((next_y, next_x))
+                    next_y += delta_y
+                    next_x += delta_x
+
+    return len(antinodes)
 
 
 def main():
--- tests/aoc_2024/solutions/test_day_08.py
+++ tests/aoc_2024/solutions/test_day_08.py
@@ -22,9 +22,8 @@ class Test(unittest.TestCase):
     def test_part_one(self):
         self.assertEqual(part_one(test_input), 14)
 
-    @unittest.skip("Not yet implemented")
     def test_part_two(self):
-        self.assertEqual(part_two(test_input), "x")
+        self.assertEqual(part_two(test_input), 34)
 
 
 def main():
